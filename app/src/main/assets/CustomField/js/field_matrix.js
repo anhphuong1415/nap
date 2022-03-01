a1bb5b31f460d67fe36dd9c3ba218ce1ead82e83
nganphuong1508 <56165117+nganphuong1508@users.noreply.github.com>
2022-03-01 15:50:34 +0700

    fix matrix field(complete)

--- app/src/main/assets/CustomField/js/field_matrix.js
+++ app/src/main/assets/CustomField/js/field_matrix.js
@@ -15,14 +15,14 @@ var CustomFields = CustomFields || {};
 
 CustomFields.FieldMatrix = function(R1, R2, R3, R4, R5, R6, R7, R8, opt_validator) {
     var value = {};
-    value[0] = R1 || 0x00;
-    value[1] = R2 || 0x00;
-    value[2] = R3 || 0x00;
-    value[3] = R4 || 0x00;
-    value[4] = R5 || 0x00;
-    value[5] = R6 || 0x00;
-    value[6] = R7 || 0x00;
-    value[7] = R8 || 0x00;
+    value.R1 = R1 || 0x00;
+    value.R2 = R2 || 0x00;
+    value.R3 = R3 || 0x00;
+    value.R4 = R4 || 0x00;
+    value.R5 = R5 || 0x00;
+    value.R6 = R6 || 0x00;
+    value.R7 = R7 || 0x00;
+    value.R8 = R8 || 0x00;
 
     CustomFields.FieldMatrix.superClass_.constructor.call(this, value, opt_validator);
     this.size_ = new Blockly.utils.Size(0, 0);
@@ -61,10 +61,29 @@ CustomFields.FieldMatrix.prototype.initView = function() {
 }
 
 CustomFields.FieldMatrix.prototype.doClassValidation_ = function(newValue) {
-    for (var i = 0; i < 8; i++) {
-        if (newValue[0] == undefined || newValue[0] > 255 || newValue[0] < 0) {
-            newValue[0] = 0x00;
-        }
+    if (newValue.R1 == undefined || newValue.R1 > 255 || newValue.R1 < 0) {
+        newValue.R1 = 0x00;
+    }
+    if (newValue.R2 == undefined || newValue.R2 > 255 || newValue.R2 < 0) {
+        newValue.R2 = 0x00;
+    }
+    if (newValue.R3 == undefined || newValue.R3 > 255 || newValue.R3 < 0) {
+        newValue.R3 = 0x00;
+    }
+    if (newValue.R4 == undefined || newValue.R4 > 255 || newValue.R4 < 0) {
+        newValue.R4 = 0x00;
+    }
+    if (newValue.R5 == undefined || newValue.R5 > 255 || newValue.R5 < 0) {
+        newValue.R5 = 0x00;
+    }
+    if (newValue.R6 == undefined || newValue.R6 > 255 || newValue.R6 < 0) {
+        newValue.R6 = 0x00;
+    }
+    if (newValue.R7 == undefined || newValue.R7 > 255 || newValue.R7 < 0) {
+        newValue.R7 = 0x00;
+    }
+    if (newValue.R8 == undefined || newValue.R8 > 255 || newValue.R8 < 0) {
+        newValue.R8 = 0x00;
     }
     return newValue;
 }
@@ -132,7 +151,9 @@ CustomFields.FieldMatrix.prototype.onMouseDown = function(event) {
 CustomFields.FieldMatrix.prototype.onMouseMove = function(event) {
     var mouseX;
     var mouseY;
-    // var arrayLed = this.value_;
+    var arrayLed = [this.value_.R1, this.value_.R2, this.value_.R3, this.value_.R4,
+        this.value_.R5, this.value_.R6, this.value_.R7, this.value_.R8
+    ];
 
     if (CustomFields.FieldVelocity.prototype.detectMobile() == "desktop") {
         mouseX = event.pageX; //get mouse's x global position
@@ -148,15 +169,23 @@ CustomFields.FieldMatrix.prototype.onMouseMove = function(event) {
         var c = parseInt(curEl.id[3]);
         if (this.curEl_.style.backgroundColor == 'transparent' || !this.curEl_.style.backgroundColor) {
             this.curEl_.style.backgroundColor = '#a8d8f8';
-            this.value_[r - 1] = this.value_[r - 1] | (1 << (c - 1));
+            arrayLed[r - 1] = arrayLed[r - 1] | (1 << (c - 1));
         } else {
             this.curEl_.style.backgroundColor = 'transparent'
-            this.value_[r - 1] = this.value_[r - 1] & ~(1 << (c - 1));
+            arrayLed[r - 1] = arrayLed[r - 1] & ~(1 << (c - 1));
         }
 
-        //FIXME: change inblock view
         this.isDirty_ = true;
-        this.setValue(this.value_);
+        var value = {};
+        value.R1 = arrayLed[0];
+        value.R2 = arrayLed[1];
+        value.R3 = arrayLed[2];
+        value.R4 = arrayLed[3];
+        value.R5 = arrayLed[4];
+        value.R6 = arrayLed[5];
+        value.R7 = arrayLed[6];
+        value.R8 = arrayLed[7];
+        this.setValue(value);
     }
 }
 
@@ -184,26 +213,89 @@ CustomFields.FieldMatrix.prototype.updateSize_ = function() {
 CustomFields.FieldMatrix.prototype.render_ = function() {
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
-            if (this.value_[i] & (1 << j)) {
+            var value;
+            switch (i) {
+                case 0:
+                    value = this.value_.R1;
+                    break;
+                case 1:
+                    value = this.value_.R2;
+                    break;
+                case 2:
+                    value = this.value_.R3;
+                    break;
+                case 3:
+                    value = this.value_.R4;
+                    break;
+                case 4:
+                    value = this.value_.R5;
+                    break;
+                case 5:
+                    value = this.value_.R6;
+                    break;
+                case 6:
+                    value = this.value_.R7;
+                    break;
+                case 7:
+                    value = this.value_.R8;
+                    break;
+            }
+            if (value & (1 << j)) {
                 this.cellLed_[i][j].style.fill = '#a8d8f8';
             } else {
-                this.cellLed_[i][j].style.fill = '#fff';;
+                this.cellLed_[i][j].style.fill = '#fff';
             }
         }
     }
-
     this.updateSize_();
+}
+
+CustomFields.FieldMatrix.prototype.findChild = function(r, c) {
+    var cnt = r * 8 + c;
+    var result = this.matrixGroup_.firstChild;
+    while (cnt-- >= 0) {
+        if (result.nextSibling) {
+            result = result.nextSibling;
+        }
+        return null;
+    }
+    return result;
 }
 
 CustomFields.FieldMatrix.prototype.renderEditor_ = function() {
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
             var id = 'r' + (i + 1).toString() + 'c' + (j + 1).toString();
-            if (this.value_[i] & (1 << j)) {
-                // this.cellLed_[i][j].style.fill = '#a8d8f8';
+            var value;
+            switch (i) {
+                case 0:
+                    value = this.value_.R1;
+                    break;
+                case 1:
+                    value = this.value_.R2;
+                    break;
+                case 2:
+                    value = this.value_.R3;
+                    break;
+                case 3:
+                    value = this.value_.R4;
+                    break;
+                case 4:
+                    value = this.value_.R5;
+                    break;
+                case 5:
+                    value = this.value_.R6;
+                    break;
+                case 6:
+                    value = this.value_.R7;
+                    break;
+                case 7:
+                    value = this.value_.R8;
+                    break;
+            }
+            if (value & (1 << j)) {
                 document.getElementById(id).style.backgroundColor = '#a8d8f8';
             } else {
-                // this.cellLed_[i][j].style.fill = '#fff';
                 document.getElementById(id).style.backgroundColor = 'transparent';
             }
         }
@@ -217,16 +309,18 @@ CustomFields.FieldMatrix.prototype.createView_ = function() {
     }, this.fieldGroup_);
     for (var i = 0; i < 8; i++) {
         for (var j = 0; j < 8; j++) {
-            this.cellLed_[i][j] = Blockly.utils.dom.createSvgElement('rect', {
-                'class': 'cellSvg',
-                'x': j * 20 + 1,
-                'y': i * 20 + 1,
-                'width': 20,
-                'height': 20,
-                'rx': 5,
-                'ry': 5,
-                'fill': '#fff'
-            }, this.matrixGroup_);
+            if (!this.cellLed_[i][j]) {
+                this.cellLed_[i][j] = Blockly.utils.dom.createSvgElement('rect', {
+                    'class': 'cellSvg',
+                    'x': j * 20 + 1,
+                    'y': i * 20 + 1,
+                    'width': 20,
+                    'height': 20,
+                    'rx': 5,
+                    'ry': 5,
+                    'fill': '#fff'
+                }, this.matrixGroup_);
+            }
         }
     }
 }
@@ -248,6 +342,38 @@ CustomFields.FieldMatrix.prototype.widgetDispose_ = function() {
     }
     this.curEl_ = null;
 }
+
+CustomFields.FieldMatrix.prototype.toXml = function(fieldElement) {
+    fieldElement.setAttribute('R1', this.value_.R1);
+    fieldElement.setAttribute('R3', this.value_.R2);
+    fieldElement.setAttribute('R4', this.value_.R3);
+    fieldElement.setAttribute('R5', this.value_.R4);
+    fieldElement.setAttribute('R2', this.value_.R5);
+    fieldElement.setAttribute('R6', this.value_.R6);
+    fieldElement.setAttribute('R7', this.value_.R7);
+    fieldElement.setAttribute('R8', this.value_.R8);
+
+    // Always return the element!
+    return fieldElement;
+};
+
+CustomFields.FieldMatrix.prototype.fromXml = function(fieldElement) {
+    // Because we had to do custom serialization for this field, we also need
+    // to do custom de-serialization.
+
+    var value = {};
+    value.R1 = fieldElement.getAttribute('R1');
+    value.R2 = fieldElement.getAttribute('R2');
+    value.R3 = fieldElement.getAttribute('R3');
+    value.R4 = fieldElement.getAttribute('R4');
+    value.R5 = fieldElement.getAttribute('R5');
+    value.R6 = fieldElement.getAttribute('R6');
+    value.R7 = fieldElement.getAttribute('R7');
+    value.R8 = fieldElement.getAttribute('R8');
+
+    // The end goal is to call this.setValue()
+    this.setValue(value);
+};
 
 CustomFields.FieldMatrix.prototype.detectMobile = function() {
     var result = (navigator.userAgent.match(/(iphone)|(ipod)|(ipad)|(android)|(blackberry)|(windows phone)|(symbian)/i));
